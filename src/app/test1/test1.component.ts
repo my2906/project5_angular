e189996bc39d06cafbe88b6b1ea324f20955abbc
my2906 <92378417+my2906@users.noreply.github.com>
2023-03-15 11:14:32 +0700

    Update test1.component.ts

--- src/app/test1/test1.component.ts
+++ src/app/test1/test1.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
-import { TodoItem } from 'src/model/TodoItem';
-import { TodoList } from 'src/model/TodoList';
+import { TodoItem } from 'src/model/TodoItem';//import mới thể thể sử dụng được model Todoitem
+import { TodoList } from 'src/model/TodoList';//import mới thể thể sử dụng được model TodoList
 @Component({
   selector: 'app-test1',
   templateUrl: './test1.component.html',
@@ -8,7 +8,7 @@ import { TodoList } from 'src/model/TodoList';
 })
 export class Test1Component {
 
-  private list = new TodoList('Bob', [
+  private list = new TodoList('Bob', [//tạo ra 1 mảng có sản phẩm
     new TodoItem('Go for run', true),
     new TodoItem('Get flowers'),
     new TodoItem('Collect tickets'),
